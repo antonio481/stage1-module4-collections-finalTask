11407892d83f2289fa17248152244349c1e9662d
antonio481 <adanilovx@gmail.com>
2023-11-20 16:03:36 +0400

    DecisionCommi

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,16 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
-    }
-}
+        List<String>projectsForDeveloper=new ArrayList<>();
+        for (Map.Entry<String, Set<String>> e : projects.entrySet()){
+        for(Set<String> valueList : projects.values()) {
+            for(String value : valueList) {
+                if(value.equals(developer))
+                    projectsForDeveloper.add(e.getKey());
+            }}}
+        projectsForDeveloper.sort(Comparator.comparingInt(String::length));
+        return projectsForDeveloper;
+}}
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,21 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>>result=new HashMap<>();
+        ArrayList<Integer>keysLength=new ArrayList<>();
+        for (Map.Entry<String, Integer> e : sourceMap.entrySet()){
+            keysLength.add(e.getKey().length());
+        }
+        ListIterator<Integer> it = keysLength.listIterator(keysLength.size());
+        while(it.hasNext()) {
+            for (Map.Entry<String, Integer> e : sourceMap.entrySet()){
+                if(it.next()==e.getKey().length())
+                    result.put(it.next(), Collections.singleton(e.getKey()));
+            }
+        }
+        return result;
     }
 }
